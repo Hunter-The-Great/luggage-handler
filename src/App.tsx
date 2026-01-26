224b214515a8f0fa251f4d4aa0e91ca16ecf7173
Hunter-The-Great <benlowry9@gmail.com>
2026-01-26 13:50:19 -0600

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,6 @@ import {
   NativeSelectOption,
 } from "./components/ui/native-select";
 
-const ThemeContext = createContext<null | string>(null);
-
-const queryClient = new QueryClient();
-
 export function App() {
   return (
     <div className="flex flex-col justify-center items-center p-6">
@@ -29,41 +25,7 @@ export function App() {
   );
 }
 
-const Todos = () => {
-  const { todos, addTodo, isLoading } = useTodos();
-  const [input, setInput] = useState("");
-  const toggleTodo = (id: string) => {};
-  return (
-    <>
-      <label>Todo Name</label>
-      <input
-        value={input}
-        className="border"
-        onChange={(e) => setInput(e.target.value)}
-      />
-
-      <button onClick={() => addTodo.mutate(input)}>Add Todo</button>
-      {isLoading ? <div>Loading...</div> : null}
-      <div>
-        {todos.map((todo) => (
-          <Todo toggleTodo={toggleTodo} todo={todo} />
-        ))}
-      </div>
-    </>
-  );
-};
-
-const HelloWorld = (props: { name: string }) => {
-  const mode = useContext(ThemeContext);
-  if (!mode) {
-    throw new Error("ThemeContext is not defined");
-  }
-  return <div>Current theme: {mode}</div>;
-};
-
 const Todo = (props: { todo: Todo; toggleTodo: (text: string) => void }) => {
-  const queryClient = useQueryClient();
-
   return (
     <div>
       <input
