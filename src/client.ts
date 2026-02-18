9faad330ebab08cb203dac39bf1cc4e4a2c1ce51
Hunter-The-Great <benlowry9@gmail.com>
2026-02-17 18:03:47 -0600

    updated url again

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden";
 import type { Api } from ".";
 
-export const client = treaty<Api>("luggage.lowryb.sbs");
+export const client = treaty<Api>("window.location.origin");
